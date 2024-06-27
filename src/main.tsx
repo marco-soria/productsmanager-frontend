b362ac7f51599c41c1806c90dcd2480c1343c437
marco-soria <marco.soriaa7@gmail.com>
2024-06-27 10:51:09 -0500

    env

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
-
+//env
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
